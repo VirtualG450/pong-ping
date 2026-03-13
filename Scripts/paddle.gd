04994ae9f6f8b982676c7466470d55b4df0b2e65
VirtualG450 <149188457+VirtualG450@users.noreply.github.com>
2026-03-13 02:39:18 +0100

    1.0.0 Control de raton e iconos.
    Añadido control mediante posicion del raton para jugar solo con raton o touchscreen.
    
    Añadido iconos del juego para windows y web.

--- Scripts/paddle.gd
+++ Scripts/paddle.gd
@@ -3,10 +3,14 @@ extends StaticBody2D
 @onready var paddle_texture : TextureRect = $TextureRect
 var texture_y := 0.0
 var screen_y := 0.0
+# Settings
 var speed := 300.0
 # Animations
 var tween1 : Tween
 var time1 := 0.1
+# Switch to mouse input
+var time2 := 2.0
+var elapsed1 := 0.0
 
 # Loading
 
@@ -23,10 +27,29 @@ func _process(delta):
 	_check_input(delta)
 
 func _check_input(delta:float) -> void:
+	## Keyboard input with W and S
 	var axis := Input.get_axis("player_1_up","player_1_down")
 	if axis:
 		global_position.y += axis * speed * delta
 		global_position.y = clampf(global_position.y, texture_y, screen_y - texture_y)
+		elapsed1 = 0
+		return
+	
+	# Only switch to mouse input after no keyboard detected for time2 seconds.
+	if elapsed1 < time2:
+		elapsed1 += delta
+		return
+	## Mouse input with mouse's position.
+	# Prepare value
+	var target_y : float = get_global_mouse_position().y - global_position.y
+	# Move towards target y direction
+	if abs(target_y) > speed * delta:
+		# Speed * (-1 or 1)
+		global_position.y += speed * delta * (target_y / abs(target_y))
+	else:
+		global_position.y += target_y
+	# Limit position to screen
+	global_position.y = clampf(global_position.y, texture_y, screen_y - texture_y)
 
 # Hit effect
 
