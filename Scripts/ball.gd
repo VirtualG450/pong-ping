a3084e455d96b49fbc663c2daffe6a9182f99bea
VirtualG450 <149188457+VirtualG450@users.noreply.github.com>
2026-03-14 18:19:41 +0100

    1.0.2 Speed balances
    Incrementado la velocidad inicial e incrementos de la pelota al colisionar con las barras.
    
    Limitado la velocidad para evitar incremento ilimitado.
    
    Añadido un pequeño margen en la colisión de la pelota para evitar que se quede atrapada entre una pared y la barra, se desactiva la mascara de colisión durante 0.1 segundos, lo cual es suficiente para que se mueva fuera de las barras.

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -9,13 +9,16 @@ extends CharacterBody2D
 @onready var ball_wall_hit : AudioStreamPlayer2D = $BallWallHit
 @onready var ball_paddle_hit : AudioStreamPlayer2D = $BallPaddleHit
 @onready var ball_scored : AudioStreamPlayer = $BallScored
-# Ball settings
-var accel := 20.0
-var speed := 150.0
+@onready var ball_hitbox : CollisionShape2D = $BallHitbox
+# Behaviour
+const accel := 30.0
+var speed := 0.0
 var dir := Vector2.ZERO
-# Start animation
+# Animations
 var tween1 : Tween = null
-var time1 := 0.5
+const time1 := 0.5
+# Timing
+const time2:= 0.1
 
 # Loading
 
@@ -25,7 +28,7 @@ func _ready():
 func start_ball() -> void:
 	scale = Vector2.ZERO
 	global_position = get_viewport_rect().get_center()
-	speed = 150.0
+	speed = max(180, speed - 180)
 	dir.x = [-1, 1].pick_random()
 	dir.y = randf_range(-1, 1)
 	dir = dir.normalized()
@@ -42,28 +45,50 @@ func _physics_process(delta):
 	var collision := move_and_collide(dir * speed * delta)
 	if not collision:
 		return
-	var collider : StaticBody2D = collision.get_collider()
-	# Walls = bounce
+	_check_collider_type(collision.get_collider(), collision)
+
+## Executes specific instructions for collisions.
+func _check_collider_type(collider:StaticBody2D, collision:KinematicCollision2D) -> void:
+	# Normal bounce
 	if collider == walls:
 		dir = dir.bounce(collision.get_normal())
 		ball_wall_hit.play()
-	# Paddles = bounce + speed up
-	elif collider == paddle_1 or collider == cpu_paddle:
+	
+	# Speed + Bounce to left
+	elif collider == paddle_1:
 		speed += accel
-		# Flip the ball x direction and bounce using the y angle from the paddle
-		dir.x = -dir.x
+		dir.x = -absf(dir.x)
 		dir.y = collider.global_position.direction_to(global_position).y
 		collider.impact()
 		ball_paddle_hit.play()
-	# Score = Restart position and signal point
+		# Disable collision with paddle for time2 seconds to prevent trapping the ball.
+		set_collision_mask_value(2,false)
+		await get_tree().create_timer(time2).timeout
+		set_collision_mask_value(2,true)
+	
+	# Speed + Bounce to right
+	elif collider == cpu_paddle:
+		speed += accel
+		dir.x = absf(dir.x)
+		dir.y = collider.global_position.direction_to(global_position).y
+		collider.impact()
+		ball_paddle_hit.play()
+		# Disable collision with paddle for time2 seconds to prevent trapping the ball.
+		set_collision_mask_value(2,false)
+		print(get_collision_mask_value(2))
+		await get_tree().create_timer(time2).timeout
+		set_collision_mask_value(2,true)
+		print(get_collision_mask_value(2))
+	
+	# Player scored point
 	elif collider == left_score:
 		start_ball()
 		main_node.point_scored(true)
 		ball_scored.play()
+	# CPU scored points
 	elif collider == right_score:
 		start_ball()
 		main_node.point_scored(false)
 		ball_scored.play()
-
-
-#
+	# Clamp speed.
+	speed = clampf(speed, 0, 660)
